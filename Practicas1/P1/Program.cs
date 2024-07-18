39a8c6ff04c8ec8ddd3f27745b20872e80a6094f
Dario Darius <developerdarius@hotmail.com>
2024-07-18 00:17:35 -0300

    Escriba un programa de aplicación que solicite al usuario ingresar  su nombre y apellido, su edad y su dni e imprima en la consola los  datos formateados de la siguiente manera:  Nombre y Apellido: nomyape (edad), DNI: nro de dni

---
+++ Practicas1/P1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese su nombre y Apellido:");
+            string nomyape = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad:");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su DNI:");
+            double dni = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nombre y Apellido: {0} ({1}), DNI: {2}", nomyape, edad, dni);
+
+        }
+    }
+}
