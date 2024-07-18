b06254580a88495a7152229af85b854f170216d0
Dario Darius <developerdarius@hotmail.com>
2024-07-18 00:48:57 -0300

    Escriba un programa de aplicación que calcule la suma de dos números reales introducidos por teclado y muestre el resultado en la consola.

---
+++ Practicas1/P2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese primer numero:");
+            double pri = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo numero:");
+            double seg = double.Parse(Console.ReadLine());
+
+            double suma = (pri + seg);
+            Console.WriteLine("La suma es:{0}", suma);
+
+        }
+    }
+}
