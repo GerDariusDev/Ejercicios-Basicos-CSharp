a0fa4a78e5a7edd3955942904b3850ecb6377653
Dario Darius <developerdarius@hotmail.com>
2024-07-18 02:35:03 -0300

    Escriba un programa de aplicación que solicite al usuario que ingrese un número de mes (1 a 12) e imprima el nombre del mes correspondiente. Si el valor ingresado no está en ese rango debe imprimir “Mes inválido” . Utilice la sentencia switch.

---
+++ Practicas1/P3/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace P3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese un numero del 1 al 12: ");
+            byte numero = byte.Parse(Console.ReadLine());
+
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("Enero");
+                    break;
+                case 2:
+                    Console.WriteLine("Febrero");
+                    break;
+                case 3:
+                    Console.WriteLine("Marzo");
+                    break;
+                case 4:
+                    Console.WriteLine("Abril");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayo");
+                    break;
+                case 6:
+                    Console.WriteLine("Junio");
+                    break;
+                case 7:
+                    Console.WriteLine("Julio");
+                    break;
+                case 8:
+                    Console.WriteLine("Agosto");
+                    break;
+                case 9:
+                    Console.WriteLine("Septiembre");
+                    break;
+                case 10:
+                    Console.WriteLine("Octubre");
+                    break;
+                case 11:
+                    Console.WriteLine("Noviembre");
+                    break;
+                case 12:
+                    Console.WriteLine("Diciembre");
+                    break;
+                default:
+                    Console.WriteLine("Mes invalido");
+                    break;
+            }
+            Console.ReadKey(true);
+
+        }
+    }
+}
