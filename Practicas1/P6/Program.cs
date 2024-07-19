83c5f3006728e1cef7fe6a787ddbad4cb5770d3b
Dario Darius <developerdarius@hotmail.com>
2024-07-19 03:57:30 -0300

    Implemente un programa de aplicación que permita al usuario ingresar números por la consola. A medida que se van ingresando los valores el sistema debe mostrar por la consola la suma acumulada de los mismos. El proceso termina cuando se ingresa un 0(cero). Informar al final la cantidad total de números ingresados y sumados.

---
+++ Practicas1/P6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace P6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double entrada = 1, acumulador = 0;
+            int totalnumeros = 0;
+
+            while(entrada != 0)
+            {
+                Console.WriteLine("Ingrese valor:");
+                entrada = double.Parse(Console.ReadLine());
+                if (entrada == 0) { break; }
+                acumulador += entrada;
+                totalnumeros ++;
+                Console.WriteLine("Suma de los numeros ingresados hasta el momento:{0}", acumulador);
+
+            }
+
+            Console.WriteLine("Total numeros ingresados:{0}, suma total de sus valores:{1}", totalnumeros, acumulador);
+            Console.ReadKey();
+
+        }
+    }
+}
