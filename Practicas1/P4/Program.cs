d8663ccffa34437635f409e07c8d3aae41061545
Dario Darius <developerdarius@hotmail.com>
2024-07-18 22:14:49 -0300

    Escriba un programa de aplicación que lea por teclado una secuencia de números y que imprima el valor máximo, el valor mínimo de dicho conjunto y el valor promedio.

---
+++ Practicas1/P4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int acumulador, numero, menor, mayor, vueltas;
+            Console.WriteLine("Ingrese un valor inicial distinto de 0: ");
+            numero = menor = mayor = int.Parse(Console.ReadLine());
+            acumulador = numero;
+            vueltas = 0;
+
+            while (numero != 0)
+            {
+                if (numero < menor) menor = numero;
+                if (numero > mayor) mayor = numero;
+                Console.WriteLine("Ingrese un numero ó 0 para finalizar:");
+                numero = int.Parse(Console.ReadLine());
+                acumulador = acumulador + numero;
+                vueltas++;
+            }
+
+            Console.WriteLine("Menor:" + menor + " Mayor:" + mayor);
+            Console.WriteLine("Valor promedio de numeros ingresados: " + (acumulador / vueltas));
+            Console.ReadKey(true);
+        }
+    }
+}
