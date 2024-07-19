6e197c456b9cea7ab00a515103cb34344afd37d7
Dario Darius <developerdarius@hotmail.com>
2024-07-18 22:27:53 -0300

    Escriba un programa de aplicación que imprima en la consola todos los números impares del intervalo [40,352] que además sean múltiplos de 5.

---
+++ Practicas1/P5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int x = 40; x >= 40 & x <= 352; x++)
+            {
+                if (x % 5 == 0 & x % 2 != 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadKey(true);
+
+        }
+    }
+}
