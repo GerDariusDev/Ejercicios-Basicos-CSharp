742fcd41c78e02f86af9f73001003755108774f0
Dario Darius <developerdarius@hotmail.com>
2024-07-20 01:28:49 -0300

    Escriba un programa de aplicación que lea 2 palabras y determine si son palíndromos entre ellas. (Ej: “hola” y “aloh” son palíndromos).

---
+++ Practicas2/P1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string pal1 = "", pal2 = "", compara="";
+            Console.WriteLine("Ingrese primer palabra: ");
+            pal1 = Console.ReadLine();
+            Console.WriteLine("Ingrese segunda palabra: ");
+            pal2 = Console.ReadLine();
+
+
+            if (pal1.Length == pal2.Length)
+            {
+                for (int x = (pal1.Length - 1); x >= 0; x--)
+                {
+                    compara += pal1[x];
+                }
+
+                if (compara == pal2)
+                {
+                    Console.WriteLine("Son palindromos");
+                }
+                else { Console.WriteLine("No son palindromos"); }
+
+            }
+            else { Console.WriteLine("No son palindromos"); }
+        }
+    }
+}
