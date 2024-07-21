1fe8c05aeb62d8e2f194bc9a368623b8d800f52f
Dario Darius <developerdarius@hotmail.com>
2024-07-21 04:39:28 -0300

    Haga un programa de aplicación que lea por consola una sucesión de palabras,y termina la sucesion con una palabra vacía. Imprima el porcentaje de palabras que comienzan con ‘s’, la longitud de cada palabra leída y el promedio de caracteres por palabra.

---
+++ Practicas2/P2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+namespace P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> palabras = new List<string>(); //Array unicamente para strings (System.Collections)
+            string ingreso = "x";
+            double s = 0, cantpalabras = 0, caracteres = 0, longitud, porcentaje, promedio;
+
+            while (ingreso != "")
+            {
+                Console.WriteLine("Ingrese palabra: ");
+                ingreso = Console.ReadLine();
+
+                if (ingreso == "") { break; }
+                
+                if (ingreso[0] == 's' || ingreso[0] == 'S') { s++; }
+
+                longitud = 0;
+                foreach (char puntero in ingreso)
+                {
+                    longitud++;
+                    caracteres++;
+                }
+                Console.WriteLine("Longitud de palabra ingresada:{0}", longitud);
+                
+                palabras.Append(ingreso);
+                
+                cantpalabras++;
+                
+            }
+
+            porcentaje = (s / cantpalabras) * 100;
+            promedio = caracteres / cantpalabras;
+            Console.WriteLine("Porcentaje de palabras que comienzan con S : %{0}", porcentaje);
+            Console.WriteLine("Promedio de caracteres por palabras : {0}", promedio);
+
+            Console.ReadKey();
+        }
+    }
+}
