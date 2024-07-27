40dede61bd817fc3a4bc923d4fd31a01dd371cb7
Dario Darius <developerdarius@hotmail.com>
2024-07-27 16:15:02 -0300

    Defina una función que reciba una palabra y retorne en un vector la cantidad de cada una de las vocales que contiene.

---
+++ Practicas2/P4/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ingrese una palabra:");
+            string pal = Console.ReadLine();
+            string palabra = pal.ToLower();
+
+            char[] vocalespalabra = RetornoFuncion(palabra);
+
+            foreach(char puntero in vocalespalabra)
+            {
+                if (puntero != '\0') // Evitar imprimir espacios vacios del vector
+                {
+                    Console.WriteLine("Las vocales dentro de la palabra son:{0}", puntero);
+                }
+            }
+
+
+        }
+
+        static char[] RetornoFuncion (string pal)
+        {
+            int sector = 0;
+            char[] vocales = new char[pal.Length];
+            foreach (char puntero in pal)
+            {
+                if (puntero == 'a' || puntero == 'e' || puntero == 'i' || puntero == 'o' || puntero == 'u')
+                {
+                    vocales[sector] = puntero;
+                    sector++;
+                }
+            }
+
+            return vocales;
+        }
+    }
+}
